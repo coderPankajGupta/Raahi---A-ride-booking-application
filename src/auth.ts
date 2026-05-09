a15fac0bbb8fec6f7159c30a7f2182460be1bf96
coderPankajGupta <pg3538309@gmail.com>
2026-05-10 03:15:58 +0530

    working

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import connectDB from "@/lib/connectDB";
+import userModel from "@/models/user.model";
+import bycrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -16,6 +19,66 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           placeholder: "*****",
         },
       },
+
+      async authorize(credentials, request) {
+        if (!credentials.email || !credentials.password) {
+          throw new Error("Email and password are required.");
+        }
+        await connectDB();
+        const user = await userModel.findOne({ email: credentials.email });
+        if (!user) {
+          throw new Error("No user found with the provided email.");
+        }
+        const isPasswordValid = await bycrypt.compare(
+          credentials.password as string,
+          user.password,
+        );
+        if (!isPasswordValid) {
+          throw new Error("Invalid password.");
+        }
+        return {
+          id: user._id,
+          name: user.name,
+          role: user.role,
+          email: user.email,
+        };
+      },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.name = user.name;
+        token.id = user.id;
+        token.role = user.role;
+        token.email = user.email;
+      }
+
+      return token;
+    },
+
+    async session({ token, session }) {
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+        session.user.email = token.email as string;
+      }
+
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/auth/signin",
+    error: "/auth/signin",
+  },
+
+  session: {
+    strategy: "jwt",
+    maxAge: 10 * 24 * 60 * 60,
+  },
+
+  secret: process.env.AUTH_SECRET,
 });
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,6 +4,7 @@ interface IUser extends Document {
   name: string;
   email: string;
   password?: string;
+  role: "user" | "partner" | "admin";
   createdAt: Date;
   updatedAt: Date;
 }
@@ -17,10 +18,15 @@ const userSchema = new mongoose.Schema<IUser>(
     email: {
       type: String,
       required: true,
-      unique:true
+      unique: true,
     },
     password: {
       type: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "partner", "admin"],
     },
   },
   { timestamps: true },
