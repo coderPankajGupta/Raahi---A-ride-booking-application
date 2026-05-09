26a8f1ed0f4c87fe3f9c980bd42a42a972c9b557
coderPankajGupta <pg3538309@gmail.com>
2026-05-09 19:22:18 +0530

    working on auth section.

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,9 +1,34 @@
 import connectDB from "@/lib/connectDB";
-import { NextRequest } from "next/server";
+import userModel from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req:NextRequest) {
+export async function POST(req: NextRequest) {
+  try {
     const { name, email, password } = await req.json();
-    await connectDB()
+    await connectDB();
 
-    
+    let user = await userModel.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 },
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: `Unable to create account : ${error}` },
+      { status: 500 },
+    );
+  }
 }
